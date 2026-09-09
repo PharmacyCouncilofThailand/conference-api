46135bccde33b015a4d76e53a5a990227702f779
KoonArno <nattakarnklongkratok@gmail.com>
2026-09-09 19:36:37 +0700

    fix: remove receipt signature footer

--- src/services/receipt/components/ReceiptFooter.tsx
+++ src/services/receipt/components/ReceiptFooter.tsx
@@ -12,42 +12,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: theme.colors.secondary,
   },
-  footer: {
-    marginTop: 20,
-  },
-  footerTitle: {
-    textAlign: "right",
-    marginRight: 215,
-    fontSize: 16,
-  },
-  signatureRow: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  signatureBlock: {
-    alignItems: "center",
-    width: "40%",
-  },
-  signatureParentheses: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "flex-end",
-    width: "100%",
-    marginBottom: 5,
-  },
-  signatureParen: {
-    fontSize: 16,
-    color: theme.colors.primary,
-  },
-  signatureSpace: {
-    width: 150,
-    fontSize: 16,
-  },
-  signatureLabel: {
-    fontSize: 16,
-    color: theme.colors.primary,
-    textAlign: "center",
-  },
 });
 
 interface ReceiptFooterProps {
@@ -70,30 +34,6 @@ const ReceiptFooter: React.FC<ReceiptFooterProps> = ({
           ชำระโดย : {paymentMethod} วันที่ชำระ {paymentDate} เวลา {paymentTime} น.
         </Text>
         <Text style={styles.paymentNote}>{footerNote}</Text>
-      </View>
-
-      <View style={styles.footer}>
-        <Text style={styles.footerTitle}>ผู้รับเงิน</Text>
-        <View style={styles.signatureRow}>
-          <View style={styles.signatureBlock}>
-            <View style={styles.signatureParentheses}>
-              <Text style={styles.signatureParen}>(</Text>
-              <Text style={styles.signatureSpace}> </Text>
-              <Text style={styles.signatureParen}>)</Text>
-            </View>
-            <Text style={styles.signatureLabel}>เหรัญญิกสภาเภสัชกรรม</Text>
-          </View>
-          <View style={styles.signatureBlock}>
-            <View style={styles.signatureParentheses}>
-              <Text style={styles.signatureParen}>(</Text>
-              <Text style={styles.signatureSpace}> </Text>
-              <Text style={styles.signatureParen}>)</Text>
-            </View>
-            <Text style={styles.signatureLabel}>
-              เจ้าหน้าที่การเงินสภาเภสัชกรรม
-            </Text>
-          </View>
-        </View>
       </View>
     </>
   );
